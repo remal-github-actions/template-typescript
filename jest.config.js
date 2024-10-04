568649e9dbcce17078cde4731638bc9303130a7b
Semyon Levin <levin.semen@gmail.com>
2024-10-03 18:46:24 -0700

    code formatting

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,34 @@
 export default {
-  clearMocks: true,
-  moduleFileExtensions: ['ts', 'js', 'mjs'],
-  testEnvironment: 'node',
-  testRunner: 'jest-circus/runner',
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
-  },
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { 'useESM': true }]
-  },
-  testMatch: [
-    '**/*.spec.(ts|js|mjs)'
-  ],
-  testPathIgnorePatterns: [
-    '/build/',
-    '/dist/',
-    '/node_modules/'
-  ],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**'
-  ],
-  errorOnDeprecated: true,
-  verbose: true,
-  setupFiles: [
-    '<rootDir>/jest-setup-env.js'
-  ],
-  setupFilesAfterEnv: [
-    '<rootDir>/jest-setup.js'
-  ],
-  testTimeout: 5000
+    clearMocks: true,
+    moduleFileExtensions: ['ts', 'js', 'mjs'],
+    testEnvironment: 'node',
+    testRunner: 'jest-circus/runner',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1'
+    },
+    transform: {
+        '^.+\\.ts$': ['ts-jest', { 'useESM': true }]
+    },
+    testMatch: [
+        '**/*.spec.(ts|js|mjs)'
+    ],
+    testPathIgnorePatterns: [
+        '/build/',
+        '/dist/',
+        '/node_modules/'
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**'
+    ],
+    errorOnDeprecated: true,
+    verbose: true,
+    setupFiles: [
+        '<rootDir>/jest-setup-env.js'
+    ],
+    setupFilesAfterEnv: [
+        '<rootDir>/jest-setup.js'
+    ],
+    testTimeout: 5000
 }
